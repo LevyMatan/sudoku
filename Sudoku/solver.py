d40abd465f9c7e3f415d459f5df36f02faf15fc2
LevyMatan <levymatanlevy@gmail.com>
2018-11-17 18:30:27 +0200

    docmunted functions

--- Sudoku/solver.py
+++ Sudoku/solver.py
@@ -12,7 +12,7 @@ class Board:
 
 
     def __init__(self):
-        self.values = np.full((9, 9), " ")
+        self.values = np.full((9, 9), None)
 
 
     def __getitem__(self, key):
@@ -20,39 +20,83 @@ class Board:
 
 
     def fill(self, index, value):
-        """ index is a tuple containing where we would like to put the value"""
+        """ 
+        fill inserts a value to the cell by its index, 
+        while making sure the number inserted obeys the rules
+        of the game:
+        * The value is in the range(1,10)
+        * The given value doesn't appear already in the same raw, 
+          column or square
+        * The given cell is not already filled
+
+        input: 
+        index - (i,j) tuple, while i,j in range(0,9) 
+
+        output:
+        ["OK"] - if the value inserted successfully
+        ["row", i] - if the value already apears in the row
+        ["column", j] - if the value already apears in the column
+        ["square", value, (i//3)*3 + j//3)] - if the value already apears in the sqaure
+        ["invalid"] -  if the value is not in the allowed range]
+        ["taken", (i,j)] - if the cell we are trying to fill is filled
+        
+        $ example
+        -- fill((0,3), 7)
+        """
         i, j = index
 
         if not (value in Board.valid_values):
             raise Exception("You are trying to fill the board with a not valid value {}".format(value))
+            # return ["invalid"]
         
         
-        if self.values[i,j] != " ":
+        if self.values[i,j]:
             raise Exception("You are trying to fill a cell which is already has value!")
+            # return ["taken", (i,j)]
+        
+        # check for value in the given row
+        if value in self.values[i,:]:
+            raise Exception("The value {} already apears in the {}'th row".format(value, i))
+            # return ["row", i]
+
+
+        # check for value in the given column
+        if value in self.values[:,j]:
+            raise Exception("The value {} already apears in the {}'th column".format(value, j))
+            # return ["column", j]
+
+        # check for value in the given square
+        if value in self.values[i-i%3 : i-i%3+3, j-j%3 : j-j%3+3]:
+            raise Exception("The value {} already apears in the {}'th square".format(value, (i//3)*3 + j//3))
+            # return ["square", (i//3)*3 + j//3)]
         
         
-        self.values[i,j] = str(value)
+        self.values[i,j] = value
+        # return ["OK"]
 
 
     def __str__(self):
-        b = self.values
+        b = self.values.copy()
+        b = b.astype(str)
+        b[b == "None"] = " "
+    
         return """ 
         {} | {} | {} || {} | {} | {} || {} | {} | {} 
-        __________________________________
-        {} | {} | {} || {} | {} | {} || {} | {} | {}
-        __________________________________
+        ==================================
         {} | {} | {} || {} | {} | {} || {} | {} | {}
         ==================================
-        {} | {} | {} || {} | {} | {} || {} | {} | {} 
-        __________________________________
         {} | {} | {} || {} | {} | {} || {} | {} | {}
         __________________________________
+        {} | {} | {} || {} | {} | {} || {} | {} | {} 
+        ==================================
         {} | {} | {} || {} | {} | {} || {} | {} | {}
         ==================================
-        {} | {} | {} || {} | {} | {} || {} | {} | {} 
-        __________________________________
         {} | {} | {} || {} | {} | {} || {} | {} | {}
         __________________________________
+        {} | {} | {} || {} | {} | {} || {} | {} | {} 
+        ==================================
+        {} | {} | {} || {} | {} | {} || {} | {} | {}
+        ==================================
         {} | {} | {} || {} | {} | {} || {} | {} | {}
         """.format(
             b[0,0], b[0,1], b[0,2], b[0,3], b[0,4], b[0,5], b[0,6], b[0,7], b[0,8],
@@ -66,6 +110,8 @@ class Board:
             b[8,0], b[8,1], b[8,2], b[8,3], b[8,4], b[8,5], b[8,6], b[8,7], b[8,8]
             )
         
-        
+
+    def validate_row(self, index):
+        assert self.values.unique()
 
         
